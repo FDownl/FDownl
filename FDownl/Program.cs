cae5901014f70537d1b8ef9c6294e8b60112ee65
Et3rnos <57318423+Et3rnos@users.noreply.github.com>
2021-08-06 12:26:33 +0100

    Revert "Disabled Kestrel HTTPS on main app"

--- FDownl/Program.cs
+++ FDownl/Program.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using System.Net;
 
 namespace FDownl
 {
@@ -21,7 +20,6 @@ namespace FDownl
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseKestrel(x => x.Listen(IPAddress.Loopback, 5000));
                     webBuilder.UseStartup<Startup>();
                 });
     }
