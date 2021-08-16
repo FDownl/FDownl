64cb4192154e17de3d53663a3304c70c7ac5bd88
mcdulltii <mcdulltii@gmail.com>
2021-08-16 16:58:51 +0800

    Added password field in SQL

--- Fdownl Storage/Controllers/UploadController.cs
+++ Fdownl Storage/Controllers/UploadController.cs
@@ -124,6 +124,7 @@ namespace Fdownl_Storage.Controllers
             string coupon = uploadForm.Code;
             int lifetime = uploadForm.Lifetime;
             string password = uploadForm.Password;
+            bool isEncrypted = !String.IsNullOrEmpty(password);
 
             long fileSize = uploadForm.Files.Sum(x => x.Length);
 
@@ -135,7 +136,7 @@ namespace Fdownl_Storage.Controllers
             CreateDirectoryIfNotExists(tempUploadPath);
             string fullSavePath = Path.Combine(mainUploadPath, filename);
 
-            if (uploadForm.Files.Count == 1)
+            if (uploadForm.Files.Count == 1 && !isEncrypted)
             {
                 using var stream = new FileStream(fullSavePath, FileMode.Create);
                 await uploadForm.Files.First().CopyToAsync(stream);
@@ -152,6 +153,7 @@ namespace Fdownl_Storage.Controllers
                     await file.CopyToAsync(stream);
                 }
                 using (ZipFile zip = new ZipFile()) {
+                    // Encryption method set as AES-256
                     zip.Encryption = EncryptionAlgorithm.WinZipAes256;
                     zip.Password = password;
                     zip.AddDirectory(tempFolder);
@@ -170,6 +172,7 @@ namespace Fdownl_Storage.Controllers
                 Lifetime = lifetime,
                 Ip = ip,
                 Size = fileSize,
+                IsEncrypted = isEncrypted,
                 Coupon = coupon
             };
             _databaseContext.UploadedFiles.Add(uploadedFile);
