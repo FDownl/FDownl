67c3a82a00ae677e29cb9a48e6786aeed59d322a
Et3rnos <57318423+Et3rnos@users.noreply.github.com>
2021-10-28 10:31:45 +0100

    Update script.js

--- src/FDownl/wwwroot/js/script.js
+++ src/FDownl/wwwroot/js/script.js
@@ -159,7 +159,8 @@ function getHistory() {
 					var timeToDeletion = "";
 					var lifetime = res[i].lifetime;
 					var uploadedAt = new Date(res[i].uploadedAt + "Z");
-					var deletedAt = new Date(); deletedAt.setSeconds(uploadedAt.getSeconds() + lifetime + 60);
+					var deletedAt = new Date();
+					deletedAt.setSeconds(uploadedAt.getSeconds() + lifetime + 60);
 					var time = Math.floor(deletedAt - new Date()) / 1000;
 					var days = Math.floor(time / (60*60*24));
 					var hours = Math.floor((time / (60*60)) % 24);
