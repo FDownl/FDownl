21c20d6b28085643a7fc4867ba2d1c0ece5d5a6d
Aaron <mcdulltii@gmail.com>
2021-11-11 09:39:08 +0800

    Update preview.js
    Added quotation mark replacement

--- src/FDownl/wwwroot/js/preview.js
+++ src/FDownl/wwwroot/js/preview.js
@@ -8,13 +8,22 @@ function sanitize(string) {
 		'&': '&amp;',
 		'<': '&lt;',
 		'>': '&gt;',
-		'"': '&quot;',
-		"'": '&#x27;',
 		"/": '&#x2F;',
 		"`": '&grave;',
 	};
-	const reg = /[&<>"'`/]/ig;
-	return string.replace(reg, (match)=>(map[match]));
+	const reg = /[&<>`/]/ig;
+	return string.replace(reg, (match)=>(map[match]))                            // mapped symbols
+	.replace(/'''/g, '\u2034')                                                   // triple prime
+	.replace(/(\W|^)"(\S)/g, '$1\u201c$2')                                       // beginning "
+	.replace(/(\u201c[^"]*)"([^"]*$|[^\u201c"]*\u201c)/g, '$1\u201d$2')          // ending "
+	.replace(/([^0-9])"/g,'$1\u201d')                                            // remaining " at end of word
+	.replace(/''/g, '\u2033')                                                    // double prime
+	.replace(/(\W|^)'(\S)/g, '$1\u2018$2')                                       // beginning '
+	.replace(/([a-z])'([a-z])/ig, '$1\u2019$2')                                  // conjunction's possession
+	.replace(/((\u2018[^']*)|[a-z])'([^0-9]|$)/ig, '$1\u2019$3')                 // ending '
+	.replace(/(\u2018)([0-9]{2}[^\u2019]*)(\u2018([^0-9]|$)|$|\u2019[a-z])/ig, '\u2019$2$3')     // abbrev. years like '93
+	.replace(/(\B|^)\u2018(?=([^\u2019]*\u2019\b)*([^\u2019\u2018]*\W[\u2019\u2018]\b|[^\u2019\u2018]*$))/ig, '$1\u2019') // backwards apostrophe
+	.replace(/'/g, '\u2032');
 }
 
 /* Retrieve file preview */
