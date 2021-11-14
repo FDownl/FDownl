f5c64f1c293e750dd735c9091b6f01f236ea364a
Et3rnos <57318423+Et3rnos@users.noreply.github.com>
2021-11-14 12:42:27 +0000

    fixed public feature

--- src/FDownl/wwwroot/js/script.js
+++ src/FDownl/wwwroot/js/script.js
@@ -223,7 +223,7 @@ $("#upload_btn").click(function (e) {
 	$("#progress-wrp").fadeIn(1000);
 	formData.set("lifetime", document.getElementById("lifetime").value);
 	formData.set("code", document.getElementById("coupon_code").value);
-	formData.set("public", document.getElementById("public").value);
+	formData.set("public", document.getElementById("public").checked);
 	if (document.getElementById("isEncrypted").checked) {
 		formData.set("password", document.getElementById("password").value);
 	} else {
